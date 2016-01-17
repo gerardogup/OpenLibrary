e259b5c5ff6789406ed59f7ed32b0bdf17c8aac5
Gerardo Guerra Pulido <gerardogup@gmail.com>
2016-01-17 00:47:46 -0600

    Initial Commit

---
+++ OpenLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenLibrary
+//
+//  Created by Gerardo Guerra Pulido on 17/01/16.
+//  Copyright © 2016 Gerardo Guerra Pulido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
